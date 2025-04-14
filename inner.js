c2c546e7f75cef3770a432ec203dd2cd59693b22
mint <60362123+mintexists@users.noreply.github.com>
2025-04-14 03:41:25 -0400

    starting to rename shit

--- inner.js
+++ inner.js
@@ -11,7 +11,7 @@ export default (() => {
 // store the code for the worker module as a function that takes the shared module exports as an argument
 const __dcg_worker_module__ = (__dcg_shared_module_exports__) => {
 ` +
-      "var n={};__dcg_shared_module_exports__['qe'].log=function(e){self.postMessage({log:JSON.stringify(e)})};var o=self;o.window=o;o.onmessage=function(e){let s=e.data&&e.data.connectionId;if(s)if(e.data.originalMessage.type===\"destroy\")delete n[s];else{let a=n[s];a||(a=new __dcg_shared_module_exports__['re']((r,g)=>{o.postMessage({connectionId:s,originalMessage:{type:r,payload:g}})}),n[s]=a),a.processChangeSet(e.data.originalMessage)}};o.loadMessageQueue&&(o.loadMessageQueue.forEach(e=>{o.onmessage(e)}),delete o.loadMessageQueue);\n" +
+      xhrFetch('./worker.js') +
       `
 };
 // execute the shared module store its exports
--- worker.js
+++ worker.js
@@ -1,33 +1,26 @@
-// store the code for the worker module as a function that takes the shared module exports as an argument
-const __dcg_worker_module__ = (__dcg_shared_module_exports__) => {
-  var n = {};
-  __dcg_shared_module_exports__["qe"].log = function (e) {
-    self.postMessage({ log: JSON.stringify(e) });
-  };
-  var o = self;
-  o.window = o;
-  o.onmessage = function (e) {
-    let s = e.data && e.data.connectionId;
-    if (s) {
-      if (e.data.originalMessage.type === "destroy") delete n[s];
-      else {
-        let a = n[s];
-        a || (a = new __dcg_shared_module_exports__["re"]((r, g) => {
-          o.postMessage({
-            connectionId: s,
-            originalMessage: { type: r, payload: g },
-          });
-        }),
-          n[s] = a), a.processChangeSet(e.data.originalMessage);
-      }
-    }
-  };
-  o.loadMessageQueue && (o.loadMessageQueue.forEach((e) => {
-    o.onmessage(e);
-  }),
-    delete o.loadMessageQueue);
+var n = {};
+__dcg_shared_module_exports__["qe"].log = function (e) {
+  self.postMessage({ log: JSON.stringify(e) });
 };
-// execute the shared module store its exports
-const __dcg_worker_shared_module_exports__ = `${__dcg_shared_module_source__};`;
-// call the worker module, passing in the shared module exports
-__dcg_worker_module__(__dcg_worker_shared_module_exports__);
+var o = self;
+o.window = o;
+o.onmessage = function (e) {
+  let s = e.data && e.data.connectionId;
+  if (s) {
+    if (e.data.originalMessage.type === "destroy") delete n[s];
+    else {
+      let a = n[s];
+      a || (a = new __dcg_shared_module_exports__["re"]((r, g) => {
+        o.postMessage({
+          connectionId: s,
+          originalMessage: { type: r, payload: g },
+        });
+      }),
+        n[s] = a), a.processChangeSet(e.data.originalMessage);
+    }
+  }
+};
+o.loadMessageQueue && (o.loadMessageQueue.forEach((e) => {
+  o.onmessage(e);
+}),
+  delete o.loadMessageQueue);
